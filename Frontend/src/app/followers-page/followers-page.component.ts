da17a7f5b54daeac65f13a662268ec246a47ac0d
Frederico <fredprazeres10@gmail.com>
2023-05-10 10:33:30 +0100

    US15 Implementado.

---
+++ Frontend/src/app/followers-page/followers-page.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-followers-page',
+  templateUrl: './followers-page.component.html',
+  styleUrls: ['./followers-page.component.css']
+})
+export class FollowersPageComponent implements OnInit {
+  followers:string[]=[];
+  name:string="";
+
+  constructor(
+    private route: ActivatedRoute,
+    private userService:UserService,
+  ) {}
+
+  ngOnInit(): void {
+    const userName = this.route.snapshot.paramMap.get('name')!;
+    this.name=userName;
+    this.userService.getUser(userName).pipe().subscribe(res=>{
+      this.followers=res.followerLists;
+    });
+  }
+  dashboard() {
+    this.userService.routeHere('/dashboard');
+  }
+}
